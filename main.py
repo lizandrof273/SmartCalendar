6f3fb15eacb7a1ce95059c75b97951ecfc9fdbda
Lizandro Franco <lizandrof273@gmail.com>
2018-08-01 09:01:04 -0700

    worked on knowing the days

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import os
 from google.appengine.api import users
 import urllib
 from google.appengine.ext import ndb
+import time
+import datetime
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -38,6 +40,70 @@ class Event(ndb.Model):
 
 #Event(organizer=user.user_id(), title="CSSI Presentations")
 
+current_day_of_week = datetime.date.today().strftime("%A")
+  if current_day_of_week == Monday:
+      date = datetime.datetime.now().strftime("%y%m%d")
+      mon_date = int(date) +1
+      tue_date = int(date) +2
+      wed_date = int(date) +3
+      thur_date = int(date) +4
+      fri_date = int(date) +5
+      sat_date = int(date) +6
+      sun_date = date
+  if current_day_of_week == Monday:
+      date = datetime.datetime.now().strftime("%y%m%d")
+      mon_date = date
+      tue_date = int(date) +1
+      wed_date = int(date) +2
+      thur_date = int(date) +3
+      fri_date = int(date) +4
+      sat_date = int(date) +5
+      sun_date = int(date) +6
+  if current_day_of_week == Tuesday:
+      date = datetime.datetime.now().strftime("%y%m%d")
+      mon_date = int(date) +6
+      tue_date = date
+      wed_date = int(date) +1
+      thur_date = int(date) +2
+      fri_date = int(date) +3
+      sat_date = int(date) +4
+      sun_date = int(date) +5
+  if current_day_of_week == Wednesday:
+      date = datetime.datetime.now().strftime("%y%m%d")
+      mon_date = int(date) +5
+      tue_date = int(date) +6
+      wed_date = date
+      thur_date = int(date) +1
+      fri_date = int(date) +2
+      sat_date = int(date) +3
+      sun_date = int(date) +4
+  if current_day_of_week == Thursday:
+      date = datetime.datetime.now().strftime("%y%m%d")
+      mon_date = int(date) +4
+      tue_date = int(date) +5
+      wed_date = int(date) +6
+      thur_date = date
+      fri_date = int(date) +1
+      sat_date = int(date) +2
+      sun_date = int(date) +3
+  if current_day_of_week == Friday:
+      date = datetime.datetime.now().strftime("%y%m%d")
+      mon_date = int(date) +3
+      tue_date = int(date) +4
+      wed_date = int(date) +5
+      thur_date = int(date) +6
+      fri_date = date
+      sat_date = int(date) +1
+      sun_date = int(date) +2
+  if current_day_of_week == Saturday:
+      date = datetime.datetime.now().strftime("%y%m%d")
+      mon_date = int(date) +2
+      tue_date = int(date) +3
+      wed_date = int(date) +4
+      thur_date = int(date) +5
+      fri_date = int(date) +6
+      sat_date = date
+      sun_date = int(date) +1
 app = webapp2.WSGIApplication([
     ('/', EnterInfoHandler),
 ], debug = True)
